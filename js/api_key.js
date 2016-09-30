d8fee216f6c90bb7832ac27ce12b12cb1c58781a
cjang5 <chrismangggg@gmail.com>
2016-09-30 00:04:48 -0500

    Removed API keys

--- js/api_key.js
+++ js/api_key.js
@@ -1,2 +1,2 @@
-const API_KEY_GOOGLE = 'AIzaSyBryzMtLq0LKgqkCzYcvE5EPk8pczlF7M4';
-const API_KEY_LOB    = 'test_f15eb5a9089547c6bfb922419a7cd72c0ba';
+const API_KEY_GOOGLE = '';
+const API_KEY_LOB    = '';
